a39d72cd23865ae90e6b15336a3626a720997d4a
Herman Kjøsterud Erlingsen <herman.kjosterud.erlingsen@gmail.com>
2024-11-05 15:23:55 +0100

    endring

--- index.py
+++ index.py
@@ -1,31 +1,40 @@
 # importing speechToText
 from speechToText import *
 
-# caller speechToText
-speechToText()
-
-
 # importer Yake, keyword extractor
 import yake 
 
+# caller speechToText
+speechToText()
+
+# ---------------henter ut keywords---------------
+
 f = open('input.txt', 'r')
 
-nøkkelord = ['weather', 'spotify']
+yrnøkkelord = ['weather', 'forecast']
 kw_extractor = yake.KeywordExtractor()
 keywords = kw_extractor.extract_keywords(f.read())
 
-for keywords in nøkkelord:
+""" for keywords in nøkkelord:
     print(keywords)
+    #calle spotify """
+while (1):
+    if keywords in yrnøkkelord:
+        print('yo')
+        #calle yr
     
     
-# henter ut keywords
+
+# --------------------operere basert på keywords--------------------
+
+    # -----------------Værmelding fra YR-----------------
+def weatherForecast():
+    return
+
+    # -------------------Spotify-------------------
 
 
-# operere basert på keywords
-
-    # Værmelding fra YR
-
-    # Spotify
+    # -------------------Klokka-------------------
 
 
 # Printe resultat i output.txt# Importer dvs. API for funksjonalitet
