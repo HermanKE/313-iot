6ba52ca1c9389292cf4f1f0a43f3e87583142086
Herman Kjøsterud Erlingsen <herman.kjosterud.erlingsen@gmail.com>
2024-11-05 14:36:27 +0100

    wrappa funksjon rundt koden

--- speechToText.py
+++ speechToText.py
@@ -2,61 +2,65 @@
 import speech_recognition as sr
 import pyttsx3
 
-# Initialisere recognizer
-r = sr.Recognizer()
+#funksjon som kan calles fra Index.py
+def speechToText():
 
-# Funksjon som tar opp lyd fra bruker
-def record_text():
-    # Loop i tilfelle det blir errors
-    while(1):
-        try:
-            # Bruker mikrofonen som kilde for input
-            with sr.Microphone() as mic:
-                # Forbreder recognizer til å motta input
-                r.adjust_for_ambient_noise(mic, duration=0.2)
+    # Initialisere recognizer
+    r = sr.Recognizer()
 
-                # Hører etter brukers input
-                userAudio = r.listen(mic)
+    # Funksjon som tar opp lyd fra bruker
+    def record_text():
+        # Loop i tilfelle det blir errors
+        while(1):
+            try:
+                # Bruker mikrofonen som kilde for input
+                with sr.Microphone() as mic:
+                    # Forbreder recognizer til å motta input
+                    r.adjust_for_ambient_noise(mic, duration=0.2)
 
-                # Bruker google til å gjennkjenne audio
-                requestText = r.recognize_google(userAudio, language="NO-nb") # definere norsk språk
+                    # Hører etter brukers input
+                    userAudio = r.listen(mic)
 
-                return requestText
-        
-        # Feilmeldinger:
-        # Feil oppstod under request til Google
-        except sr.RequestError as e:
-            print("Kunne ikke requeste resutatet; {0}".format(e))
-        
-        # Skjønner ikke hva brukeren sa
-        except sr.UnknownValueError:
-            print("Skjønner ikke hva du sier, snakk tydeligere. :)")
+                    # Bruker google til å gjennkjenne audio
+                    requestText = r.recognize_google(userAudio, language="NO-nb") # definere norsk språk
 
-    return
+                    return requestText
 
-# Funksjon som tømmer input.txt
-def clear_text():
-    f = open("input.txt", "w") 
-    f.close()
+            # Feilmeldinger:
+            # Feil oppstod under request til Google
+            except sr.RequestError as e:
+                print("Kunne ikke requeste resutatet; {0}".format(e))
 
-    return  
-  
-# Funksjon som lager input.txt og skriver i den
-def input_text(text):
-    f = open("input.txt", "a") # Lager en input-fil
-    f.write(text) # Skriver tekst
-    f.write("\n") # Ny linje
-    f.close()
+            # Skjønner ikke hva brukeren sa
+            except sr.UnknownValueError:
+                print("Skjønner ikke hva du sier, snakk tydeligere. :)")
+
+        return
+
+    # Funksjon som tømmer input.txt
+    def clear_text():
+        f = open("input.txt", "w") 
+        f.close()
+
+        return  
     
-    return
+    # Funksjon som lager input.txt og skriver i den
+    def input_text(text):
+        f = open("input.txt", "a") # Lager en input-fil
+        f.write(text) # Skriver tekst
+        f.write("\n") # Ny linje
+        f.close()
 
-# Kjører funksjonen som tømmer input.txt
-clear_text() 
+        return
 
-# Loop som recorder og skriver tekst i input.txt
-# While(1) skal endres til buttonPress
-while(1):
-    text = record_text()
-    input_text(text)
+    # Kjører funksjonen som tømmer input.txt
+    clear_text() 
 
-    print("Wrote text")
+    # Loop som recorder og skriver tekst i input.txt
+    # While(1) skal endres til buttonPress
+    while(1):
+        text = record_text()
+        input_text(text)
+
+        print("Wrote text")
+
