e97da0bb40ee10d9764d40875e159c60d53c6d9f
Trym123 <trymlundby@gmail.com>
2024-11-06 14:21:31 +0100

    Klokke funksjon i boks, kan muligens legge til mer funksjonalitet om vi gidder

--- index.py
+++ index.py
@@ -1,35 +1,27 @@
 # importing speechToText
 from speechToText import *
 import keyboard
-# importer Yake, keyword extractor
 import yake 
+import datetime
 
 # caller speechToText, Må adde en type break så den ikke kjører uendelig.
-while keyboard.add_hotkey('space')
-    speechToText()
+#speechToText()
+#Denne har ingen exit funksjonalitet, så vil bare loope for alltid, må se om det er mulig og gi den exit og av og på knapp
 
 
 # ---------------henter ut keywords---------------
 
-f = open('input.txt', 'r')
+i = open('input.txt', 'r')
+o = open('output.txt', 'w')
+
 
 yrNøkkelord = ['weather', 'forecast']
 spotifyNøkkelord = ['spotify', 'play', 'artist', 'music']
+clockNøkkelord = ['time', 'clock']
+locationNøkkelord = []
 
 kw_extractor = yake.KeywordExtractor()
-keywords = kw_extractor.extract_keywords(f.read())
-
-for keyword, score in keywords:
-    if keyword in yrNøkkelord:
-        print("Hello world")
-        break
-    elif keyword in spotifyNøkkelord:
-        print('yo')
-        # Call the relevant function or execute the relevant code here
-        break  
-    
-    
-
+keywords = kw_extractor.extract_keywords(i.read())
 # --------------------operere basert på keywords--------------------
 
     # -----------------Værmelding fra YR-----------------
@@ -40,6 +32,28 @@ def weatherForecast():
 
 
     # -------------------Klokka-------------------
-
+def CurrentTime():
+    currentTime = str(datetime.datetime.now())
+    o.write("The current time is " + currentTime)
+    o.close()
+    return
 
 # Printe resultat i output.txt# Importer dvs. API for funksjonalitet
+
+for keyword, score in keywords:
+    if keyword in yrNøkkelord:
+        print("Hello world")
+        break
+
+    elif keyword in spotifyNøkkelord:
+        print('yo')
+        # Spotify api
+        break
+
+    elif keyword in locationNøkkelord:
+        # Veibilde / direcions
+        break
+
+    elif keyword in clockNøkkelord:
+        CurrentTime()
+        break
